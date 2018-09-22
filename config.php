1bf14ee72f54f1059df2a88361c7b1c3a435714a
Dariusz Ochota <ochota.dariusz@gmail.com>
2018-09-22 11:33:38 +0000

    Update config.php

--- config.php
+++ config.php
@@ -10,8 +10,8 @@
 	mysql_select_db($mysql['database']);
 	
 	$rpc = array(
-		'login' => 'RPC_login',
-		'password' => 'RPC_password',
+		'login' => 'bitcoinrpc',
+		'password' => '04b7ecf0baa90495ceb4e4090f6b2fd37eec1e9c85fac68a',
 		'ip' => '127.0.0.1',
 		'port' => '8332'
 	);
@@ -22,16 +22,16 @@
 		'full-name' => 'Bitcoin Ponzi',												// Full name of your ponzi
 		'val' => 'BTC',																				// Cryptocurrency abbreviation
 		'precision' => 4,
-		'confirmations' => 1,																	// Minimum number of confirmations to add transaction
+		'confirmations' => 3,																	// Minimum number of confirmations to add transaction
 		'min' => 0.001,																				// Minimum pay in
 		'max' => 0.25,																				// Maximum pay in
 		'income' => 0.1,																			// How much money to send - default: 0.1 - 110%
 		'fee' => 0.01,																				// Fee taken from pay in amount
 		'payout-check' => 180,																// Time between payouts
-		'ownaddress' => '1MSkXPRK293dDMD5ds6KqVtyDadDkRyanX', // Your address
+		'ownaddress' => '39pJd5AyBzb1ccaA7nznPdjZWddL9uXrQe', // Your address
 		'sendback' => false,																	// What to do with txs that are over maximum or under minimum | true - send back, false - send to your address
-		'ponziacc' => 'btc',																	// Name of daemon account
-		'address' => '1ponzisApJfHtgrwP7CrpfEgseBmPcRD4',			// Ponzi address
+		'ponziacc' => 'ponzi',																	// Name of daemon account
+		'address' => '33Q5Q6NKLDSeB8KfC8cgfVXjtSnYtusCr2',			// Ponzi address
 		'privkey' => '',																			// Needed in setup, private key of your address
 		'blockchain-addr' => 'https://blockchain.info/en/address/',
 		'blockchain-tx' => 'https://blockchain.info/en/tx/'
